6fb9f4889dfa5310100599a6f83deb0cb1278706
Carter Brainerd <0xCB@protonmail.com>
2023-03-30 00:17:46 -0400

    Initial commit

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,43 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit);
+    res.json(newTuit);
+ }
+const findTuits = (req, res) => {
+    const topic = req.query.topic;
+    if (topic) {
+        const tuitsOfTopic = tuits
+            .filter(t => t.topic === topic)
+        res.json(tuitsOfTopic)
+        return
+    }
+    res.json(tuits)
+ }
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+      (t) => t._id === tuitdIdToUpdate)
+    tuits[tuitIndex] = 
+      {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+ }
+const deleteTuit = (req, res) => { 
+    const tuitId = req.params['tid'];
+    tuits = tuits.filter(t =>
+        t._id !== tuitId);
+    res.sendStatus(200);
+}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
